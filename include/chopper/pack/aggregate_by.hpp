977c495b6f56b54c741e3aac6f2daa4114e321ad
smehringer <svenja.mehringer@gmail.com>
2020-09-29 14:29:22 +0200

    [ADD, TEST] Welcome aggregate_by and a first test for it.

---
+++ include/chopper/pack/aggregate_by.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <seqan3/std/ranges>
+#include <vector>
+
+#include <seqan3/range/views/to.hpp>
+
+#include <chopper/pack/pack_data.hpp>
+
+void sort_by(pack_data & data, size_t column_to_sort_by)
+{
+    // Note: We may only sort by extra information
+    assert(data.filenames.size() == data.kmer_counts.size());
+    assert(data.filenames.size() == data.extra_information.size());
+
+    // generate permutation of indices sorted in descinding order by the sequence lengths
+    auto permutation = std::views::iota(0u, data.filenames.size()) | seqan3::views::to<std::vector>;
+    assert(permutation.size() == data.filenames.size());
+
+    auto const & info = data.extra_information;
+    auto compare = [&info,column_to_sort_by] (auto const l, auto const r)
+        { return info[l][column_to_sort_by] < info[r][column_to_sort_by]; };
+    std::sort(permutation.begin(), permutation.end(), compare);
+
+    // apply permutation
+    for (size_t i = 0; i < permutation.size(); i++)
+    {
+        auto current = i;
+        while (i != permutation[current])
+        {
+            auto next = permutation[current];
+            std::swap(data.filenames[current], data.filenames[next]);
+            std::swap(data.kmer_counts[current], data.kmer_counts[next]);
+            std::swap(data.extra_information[current], data.extra_information[next]);
+            permutation[current] = current;
+            current = next;
+        }
+        permutation[current] = current;
+    }
+}
+
+void aggregate_by(pack_data & data, size_t column_to_aggregate_by)
+{
+    // Note: We may only aggregate by extra information
+
+    sort_by(data, column_to_aggregate_by);
+
+    assert(!data.extra_information.empty());
+    assert(!data.extra_information[column_to_aggregate_by].empty());
+
+    size_t index{};
+    std::string current_info{data.extra_information[index][column_to_aggregate_by]};
+    ++index;
+    while (index < data.extra_information.size())
+    {
+        if (current_info == data.extra_information[index][column_to_aggregate_by])
+        {
+            data.filenames[index - 1] = data.filenames[index - 1] + ";" + data.filenames[index];
+            data.kmer_counts[index - 1] = data.kmer_counts[index - 1] + data.kmer_counts[index]; // todo sum is bad
+            data.filenames.erase(data.filenames.begin() + index);
+            data.kmer_counts.erase(data.kmer_counts.begin() + index);
+            data.extra_information.erase(data.extra_information.begin() + index);
+        }
+        else
+        {
+            current_info = data.extra_information[index][column_to_aggregate_by];
+            ++index;
+        }
+    }
+}
---
+++ test/api/pack/aggregate_by_test.cpp
@@ -0,0 +1,75 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <sstream>
+#include <vector>
+
+#include <chopper/pack/pack_data.hpp>
+#include <chopper/pack/aggregate_by.hpp>
+
+#include <seqan3/test/expect_range_eq.hpp>
+
+TEST(sort_by_test, small_example)
+{
+    pack_data data;
+    data.filenames = std::vector<std::string>{"seq1", "seq2", "seq3", "seq4", "seq5"};
+    data.kmer_counts = std::vector<size_t>{100, 40, 20, 20, 5};
+    data.extra_information = std::vector<std::vector<std::string>>{{"1", "foo"},
+                                                                   {"2", "foo"},
+                                                                   {"1", "moo"},
+                                                                   {"2", "moo"},
+                                                                   {"3", "moo"}};
+    {
+        pack_data copy = data;
+        sort_by(copy, 0);
+
+        std::vector<std::string> expected_filenames{"seq1", "seq3", "seq2", "seq4", "seq5"};
+        std::vector<size_t> expected_kmer_counts{100, 20, 40, 20, 5};
+        std::vector<std::vector<std::string>> expected_extra_information{{"1", "foo"},
+                                                                         {"1", "moo"},
+                                                                         {"2", "foo"},
+                                                                         {"2", "moo"},
+                                                                         {"3", "moo"}};
+
+        EXPECT_RANGE_EQ(copy.filenames, expected_filenames);
+        EXPECT_RANGE_EQ(copy.kmer_counts, expected_kmer_counts);
+        EXPECT_RANGE_EQ(copy.extra_information, expected_extra_information);
+    }
+}
+
+TEST(aggregate_by_test, small_example)
+{
+    pack_data data;
+    data.filenames = std::vector<std::string>{"seq1", "seq2", "seq3", "seq4", "seq5"};
+    data.kmer_counts = std::vector<size_t>{100, 40, 20, 20, 5};
+    data.extra_information = std::vector<std::vector<std::string>>{{"1", "foo"},
+                                                                   {"2", "foo"},
+                                                                   {"1", "moo"},
+                                                                   {"2", "moo"},
+                                                                   {"3", "moo"}};
+    {
+        pack_data copy = data;
+        aggregate_by(copy, 0);
+
+        std::vector<std::string> expected_filenames{"seq1;seq3", "seq2;seq4", "seq5"};
+        std::vector<size_t> expected_kmer_counts{120, 60, 5};
+        std::vector<std::vector<std::string>> expected_extra_information{{"1", "foo"}, {"2", "foo"}, {"3", "moo"}};
+
+        EXPECT_RANGE_EQ(copy.filenames, expected_filenames);
+        EXPECT_RANGE_EQ(copy.kmer_counts, expected_kmer_counts);
+        EXPECT_RANGE_EQ(copy.extra_information, expected_extra_information);
+    }
+
+    {
+        pack_data copy = data;
+        aggregate_by(copy, 1);
+
+        std::vector<std::string> expected_filenames{"seq1;seq2", "seq3;seq4;seq5"};
+        std::vector<size_t> expected_kmer_counts{140, 45};
+        std::vector<std::vector<std::string>> expected_extra_information{{"1", "foo"}, {"1", "moo"}};
+
+        EXPECT_RANGE_EQ(copy.filenames, expected_filenames);
+        EXPECT_RANGE_EQ(copy.kmer_counts, expected_kmer_counts);
+        EXPECT_RANGE_EQ(copy.extra_information, expected_extra_information);
+    }
+}
