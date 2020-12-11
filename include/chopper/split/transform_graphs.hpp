dd8155bb9f375b69fc383cfb4fd3121a577aab5c
smehringer <svenja.mehringer@fu-berlin.de>
2020-12-11 17:49:10 +0100

    [MISC] Step 8: Transfer undirected edges directly without the output file and remove writing and reading of a graph.

--- include/chopper/split/transform_graphs.hpp
+++ include/chopper/split/transform_graphs.hpp
@@ -14,37 +14,6 @@
 #include <seqan3/range/views/take_until.hpp>
 #include <seqan3/range/views/take_line.hpp>
 
-template <typename graph_type>
-void seqan2_write_graph(graph_type const & gAlign, split_data const & data, batch_config const & config)
-{
-    seqan::String<seqan::String<char> > edgeMap;
-    seqan::_createEdgeAttributes(gAlign, edgeMap);
-
-    std::ofstream dotFile(config.output_graph_file);
-
-    typedef typename seqan::VertexDescriptor<graph_type>::Type TVertexDescriptor;
-    typename seqan::DirectionIterator<std::ofstream, seqan::Output>::Type iter = directionIterator(dotFile, seqan::Output());
-
-    seqan::write(iter, "/* Edges */\n");
-    typedef typename seqan::Iterator<graph_type, seqan::EdgeIterator>::Type TConstEdIter;
-    TConstEdIter itEd(gAlign);
-    for(;!seqan::atEnd(itEd);++itEd) {
-        TVertexDescriptor sc = seqan::sourceVertex(itEd);
-        TVertexDescriptor tr = seqan::targetVertex(itEd);
-        seqan::appendNumber(iter, sc);
-        seqan::_writeEdgeType(iter, gAlign, seqan::DotDrawing());
-        seqan::appendNumber(iter, tr);
-        seqan::write(iter, " [");
-        seqan::write(iter, seqan::getProperty(edgeMap, *itEd));
-        seqan::write(iter, "];\n");
-    }
-    seqan::writeValue(iter, '\n');
-
-    seqan::write(iter, "}\n");
-
-    dotFile.close();
-}
-
 /* [source] --> [target]            [target]
  *  (2,10)       (10,15)     =>      (2,15)
  *
@@ -84,51 +53,6 @@ void merge_properties_into(node_map_type            & node_map,
             target_value[idx].first = range_start;
         }
         ++idx;
-    }
-}
-
-void read_graph(lemon::ListDigraph & g,
-                std::vector<lemon::ListDigraph::Node> & nodes,
-                lemon::ListDigraph::NodeMap<std::vector<std::pair<uint32_t, uint32_t>>> & node_map,
-                std::filesystem::path const & graph_file_name,
-                split_data const & data,
-                batch_config const & config)
-{
-    std::ifstream stream{graph_file_name};
-
-    auto file_view = std::ranges::subrange<decltype(std::istreambuf_iterator<char>{stream}),
-                                           decltype(std::istreambuf_iterator<char>{})>
-                        {std::istreambuf_iterator<char>{stream},
-                         std::istreambuf_iterator<char>{}};
-
-    // read undirected edges:
-    if (!std::ranges::equal(file_view | seqan3::views::take_line, std::string{"/* Edges */"}))
-        throw std::runtime_error{"No edges :(."};
-
-    while (!seqan3::is_char<'}'>(*std::ranges::begin(file_view)))
-    {
-        char buffer[100];
-        std::ranges::copy(file_view | seqan3::views::take_line, &buffer[0]);
-
-        uint32_t source{};
-        uint32_t target{};
-
-        auto res = std::from_chars(&buffer[0], &buffer[100], source);
-        while (!seqan3::is_char<'-'>(*res.ptr)) ++res.ptr;
-        std::from_chars(res.ptr + 3, &buffer[100], target);
-
-        // plus 2 because ids were shifted when we inserted a source/sink node
-        source += 2;
-        target += 2;
-
-        // the undirected edges connect minimizer
-        if (nodes[source] != nodes[target])                   // if not combined already
-        {
-            merge_properties_into(node_map, nodes[target], nodes[source]);
-
-            g.contract(nodes[source], nodes[target], true);   // merge target into source (true to remove any loops)
-            nodes[target] = nodes[source];                    // merge target into source
-        }
     }
 }
 
@@ -240,6 +164,35 @@ void transfer_directed_edges(lemon::ListDigraph & lemon_graph,
     }
 }
 
+template <typename graph_type>
+void transfer_undirected_edges(lemon::ListDigraph & lemon_graph,
+                               std::vector<lemon::ListDigraph::Node> & nodes,
+                               lemon::ListDigraph::NodeMap<std::vector<std::pair<uint32_t, uint32_t>>> & node_map,
+                               graph_type const & seqan2_graph)
+{
+    typedef typename seqan::VertexDescriptor<graph_type>::Type TVertexDescriptor;
+    typedef typename seqan::Iterator<graph_type, seqan::EdgeIterator>::Type TConstEdIter;
+
+    for (TConstEdIter itEd(seqan2_graph); !seqan::atEnd(itEd); ++itEd)
+    {
+        TVertexDescriptor source = seqan::sourceVertex(itEd);
+        TVertexDescriptor target = seqan::targetVertex(itEd);
+
+        // plus 2 because ids were shifted when we inserted a source/sink node
+        source += 2;
+        target += 2;
+
+        // the undirected edges connect minimizer
+        if (nodes[source] != nodes[target]) // if not combined already
+        {
+            merge_properties_into(node_map, nodes[target], nodes[source]);
+
+            // merge target into source (true to remove any loops)
+            lemon_graph.contract(nodes[source], nodes[target], true);
+            nodes[target] = nodes[source]; // merge target into source
+        }
+    }
+}
 // currently through an output file
 template <typename graph_type>
 void transform_graphs(lemon::ListDigraph & g,
@@ -259,7 +212,5 @@ void transform_graphs(lemon::ListDigraph & g,
     if (config.verbose)
         seqan3::debug_stream << "[LOG] inserted " <<  lemon::countArcs(g) << " arcs into the graph." << std::endl;
 
-    seqan2_write_graph(seqan2_graph, data, config);
-
-    read_graph(g, nodes, node_map, config.output_graph_file, data, config); // also merges nodes along undirected edges
+    transfer_undirected_edges(g, nodes, node_map, seqan2_graph);
 }
