f26b1a178051d12cb06158bb6c16f262eaa6030f
smehringer <svenja.mehringer@fu-berlin.de>
2020-12-14 08:56:57 +0100

    [FEATURE] Make FPR configurable on the command line.

--- include/chopper/build/chopper_build.hpp
+++ include/chopper/build/chopper_build.hpp
@@ -24,6 +24,7 @@ void initialize_argument_parser(seqan3::argument_parser & parser, build_config &
     parser.add_option(config.traversal_path_prefix, 'p', "traversal-prefix", "Give the prefix were the traversal files are stored.");
     parser.add_option(config.k, 'k', "kmer-size", "The kmer size to build kmers.");
     parser.add_option(config.overlap, 'l', "overlap", "The overlap between split regions of the same sequence.");
+    parser.add_option(config.FPR, 'r', "false-positive-rate", "The minimum false positive rate of every IBF.");
     parser.add_option(config.output_prefix, 'o', "out-prefix", "Prefix of the output files.");
     parser.add_option(config.verbose, 'v', "verbose", "Output logging/progress information.");
 }
