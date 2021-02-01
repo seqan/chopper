62c06366de65bfc1e77593e86e81adac28445429
smehringer <svenja.mehringer@fu-berlin.de>
2021-02-01 10:20:21 +0100

    more WIP. restructure test a little and add a multi level test.

--- test/api/build/create_ibfs_from_chopper_pack_test.cpp
+++ test/api/build/create_ibfs_from_chopper_pack_test.cpp
@@ -14,27 +14,49 @@ using seqan3::operator""_dna4;
 
 struct create_ibfs_from_chopper_pack_test : public ::testing::Test
 {
-    auto count_kmers(typename seqan3::interleaved_bloom_filter<>::membership_agent & hibf_agent,
-                     typename seqan3::interleaved_bloom_filter<>::membership_agent & libf_agent,
+    auto count_kmers(typename seqan3::interleaved_bloom_filter<>::membership_agent & ibf_agent,
                      seqan3::dna4_vector const & query,
                      build_config const & config)
     {
-        std::vector<size_t> hibf_counts(hibf_agent.result_buffer.size());
-        std::vector<size_t> libf_counts(libf_agent.result_buffer.size());
+        std::vector<size_t> ibf_counts(ibf_agent.result_buffer.size());
 
         for (auto hash : query | seqan3::views::kmer_hash(seqan3::ungapped{config.k}))
         {
-            auto const & high_res_v = hibf_agent.bulk_contains(hash);
-            auto const & low_res_v = libf_agent.bulk_contains(hash);
+            auto const & result = ibf_agent.bulk_contains(hash);
 
-            for (size_t i = 0; i < high_res_v.size(); ++i)
-                hibf_counts[i] += high_res_v[i];
-
-            for (size_t i = 0; i < low_res_v.size(); ++i)
-                libf_counts[i] += low_res_v[i];
+            for (size_t i = 0; i < result.size(); ++i)
+                ibf_counts[i] += result[i];
         }
 
-        return std::make_tuple(std::move(hibf_counts), std::move(libf_counts));
+        return ibf_counts;
+    }
+
+    auto compare_counts(typename seqan3::interleaved_bloom_filter<> & ibf,
+                        seqan3::dna4_vector const & query,
+                        std::vector<std::vector<size_t>> const & bins_with_expected_counts,
+                        build_config const & config)
+    {
+        auto agent = ibf.membership_agent();
+        auto && counts = this->count_kmers(agent, query, config);
+
+        size_t expected = std::ranges::distance(query | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
+
+        std::vector<bool> without_expected_counts(counts.size(), true);
+
+        for (auto const & bins : bins_with_expected_counts)
+        {
+            size_t sum{};
+            for (auto const bin : bins)
+            {
+                without_expected_counts[bin] = false;
+                sum += counts[bin];
+            }
+            EXPECT_EQ(sum, expected);
+        }
+
+        for (size_t i = 0; i < counts.size(); ++i)
+            if (without_expected_counts[i])
+                EXPECT_EQ(counts[i], 0) << " failed for i=" << i << std::endl;
     }
 };
 
@@ -104,59 +126,17 @@ TEST_F(create_ibfs_from_chopper_pack_test, small_example_2_levels)
     auto seq2_specific = "ATATCGATCGAGCGAGGCAGGCAGCGATCGAGCGAGCGCATGCAGCGACTAGCTACGACAGCTACTATCAGCAGCGAGCG"_dna4;
     auto seq3_specific = "ATCGATCACGATCAGCGAGCGATATCTTATCGTAGGCATCGAGCATCGAGGAGCGATCTATCTATCTATCATCTATCTAT"_dna4;
 
-    auto hibf_agent = high_level_ibf.membership_agent();
-    auto libf_agent = low_level_ibf.membership_agent();
+    // UNSPECIFIC - unspecific region should be found in all bins that include a whole sequence
+    compare_counts(high_level_ibf, unspecific, {{0}, {1}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, unspecific, {{0}, {1}, {2, 3, 4}, {5}},config);
 
-    { // UNSPECIFIC - unspecific region should be found in all bins that include a whole sequence
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, unspecific, config);
+    // SEQ2 SPECIFIC
+    compare_counts(high_level_ibf, seq2_specific, {{0}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, seq2_specific, {{1}, {2, 3, 4}},config);
 
-        size_t expected = std::ranges::distance(unspecific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
-
-        EXPECT_EQ(hibf_counts[0], expected);
-        EXPECT_EQ(hibf_counts[1], expected);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
-
-        EXPECT_EQ(libf_counts[0], expected);
-        EXPECT_EQ(libf_counts[1], expected);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], expected);
-    }
-
-    { // SEQ2 SPECIFIC
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, seq2_specific, config);
-
-        size_t expected = std::ranges::distance(seq2_specific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
-
-        EXPECT_EQ(hibf_counts[0], expected);
-        EXPECT_EQ(hibf_counts[1], 0);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
-
-        EXPECT_EQ(libf_counts[0], 0);
-        EXPECT_EQ(libf_counts[1], expected);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], 0);
-    }
-
-    { // SEQ3 SPECIFIC
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, seq3_specific, config);
-
-        size_t expected = std::ranges::distance(seq3_specific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
-
-        EXPECT_EQ(hibf_counts[0], 0);
-        EXPECT_EQ(hibf_counts[1], expected);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
-
-        EXPECT_EQ(libf_counts[0], 0);
-        EXPECT_EQ(libf_counts[1], 0);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], expected);
-    }
+    // SEQ3 SPECIFIC
+    compare_counts(high_level_ibf, seq3_specific, {{1}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, seq3_specific, {{2, 3, 4}, {5}},config);
 }
 
 TEST_F(create_ibfs_from_chopper_pack_test, same_example_two_levels_but_split_bin_as_hibf_max_bin)
@@ -225,57 +205,175 @@ TEST_F(create_ibfs_from_chopper_pack_test, same_example_two_levels_but_split_bin
     auto seq2_specific = "ATATCGATCGAGCGAGGCAGGCAGCGATCGAGCGAGCGCATGCAGCGACTAGCTACGACAGCTACTATCAGCAGCGAGCG"_dna4;
     auto seq3_specific = "ATCGATCACGATCAGCGAGCGATATCTTATCGTAGGCATCGAGCATCGAGGAGCGATCTATCTATCTATCATCTATCTAT"_dna4;
 
-    auto hibf_agent = high_level_ibf.membership_agent();
-    auto libf_agent = low_level_ibf.membership_agent();
+    // UNSPECIFIC - unspecific region should be found in all bins that include a whole sequence
+    compare_counts(high_level_ibf, unspecific, {{0}, {1}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, unspecific, {{0}, {1}, {2, 3, 4}, {5}},config);
 
-    { // UNSPECIFIC - unspecific region should be found in all bins that include a whole sequence
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, unspecific, config);
+    // SEQ2 SPECIFIC
+    compare_counts(high_level_ibf, seq2_specific, {{0}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, seq2_specific, {{1}, {2, 3, 4}},config);
 
-        size_t expected = std::ranges::distance(unspecific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
+    // SEQ3 SPECIFIC
+    compare_counts(high_level_ibf, seq3_specific, {{1}, {2}, {3,4,5}, {6}},config);
+    compare_counts(low_level_ibf, seq3_specific, {{2, 3, 4}, {5}},config);
+}
 
-        EXPECT_EQ(hibf_counts[0], expected);
-        EXPECT_EQ(hibf_counts[1], expected);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
+TEST_F(create_ibfs_from_chopper_pack_test, multi_level_ibf)
+{
+    std::string seq1_filename = DATADIR"seq1.fa";
+    std::string seq2_filename = DATADIR"seq2.fa";
+    std::string seq3_filename = DATADIR"seq3.fa";
+    std::string seq12_filename = seq1_filename + ";" + seq2_filename;
+    std::string seq13_filename = seq1_filename + ";" + seq3_filename;
+    std::string seq23_filename = seq2_filename + ";" + seq3_filename;
+    std::string seq123_filename = seq1_filename + ";" + seq2_filename + ";" + seq3_filename;
 
-        EXPECT_EQ(libf_counts[0], expected);
-        EXPECT_EQ(libf_counts[1], expected);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], expected);
+    seqan3::test::tmp_filename chopper_pack_filename{"small.pack"};
+
+    // generate data files
+    {
+        std::ofstream fout{chopper_pack_filename.get_path()};
+        fout << "#HIGH_LEVEL_IBF max_bin_id:0\n"
+             << "#MERGED_BIN_0;0;0 max_bin_id:3\n"
+             << "#MERGED_BIN_0;0 max_bin_id:4\n"
+             << "#MERGED_BIN_0;1 max_bin_id:2\n"
+             << "#MERGED_BIN_0 max_bin_id:0\n"
+             << "#MERGED_BIN_1 max_bin_id:2\n"
+             << "#FILES\tBIN_INDICES\tNUMBER_OF_BINS\tEST_MAX_TB_SIZES\n"
+             << seq1_filename   << "\t0;0;0;0\t1;1;1;3\t1650;350;80;1\n"
+             << seq2_filename   << "\t0;0;0;3\t1;1;1;1\t1650;350;80;2\n"
+             << seq3_filename   << "\t0;0;0;4\t1;1;1;2\t1650;350;80;2\n"
+             << seq23_filename  << "\t0;0;0;6\t1;1;1;2\t1650;350;80;2\n"
+             << seq123_filename << "\t0;0;0;8\t1;1;1;5\t1650;350;80;2\n"
+             << seq1_filename   << "\t0;0;1\t1;1;1\t1650;350;40\n"
+             << seq2_filename   << "\t0;0;2\t1;1;1\t1650;350;50\n"
+             << seq3_filename   << "\t0;0;3\t1;1;1\t1650;350;80\n"
+             << seq12_filename  << "\t0;0;4\t1;1;1\t1650;350;100\n"
+             << seq1_filename   << "\t0;1;0\t1;1;1\t1650;400;6\n"
+             << seq2_filename   << "\t0;1;1\t1;1;1\t1650;400;7\n"
+             << seq3_filename   << "\t0;1;2\t1;1;1\t1650;400;7\n"
+             << seq12_filename  << "\t0;2\t1;1\t1650;200\n"
+             << seq13_filename  << "\t0;3\t1;1\t1650;300\n"
+             << seq23_filename  << "\t0;4\t1;1\t1650;400\n"
+             << seq1_filename   << "\t1;0\t1;1\t2000;31\n"
+             << seq2_filename   << "\t1;1\t1;1\t2000;32\n"
+             << seq12_filename  << "\t1;2\t1;2\t2000;32\n"
+             << seq23_filename  << "\t2\t1\t1200\n"
+             << seq123_filename << "\t3\t2\t1500\n";
     }
 
-    { // SEQ2 SPECIFIC
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, seq2_specific, config);
+    build_config config{};
+    config.k = 15;
+    config.chopper_pack_filename = chopper_pack_filename.get_path().string();
 
-        size_t expected = std::ranges::distance(seq2_specific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
+    build_data data{};
 
-        EXPECT_EQ(hibf_counts[0], expected);
-        EXPECT_EQ(hibf_counts[1], 0);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
+    create_ibfs_from_chopper_pack(data, config);
 
-        EXPECT_EQ(libf_counts[0], 0);
-        EXPECT_EQ(libf_counts[1], expected);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], 0);
-    }
+    EXPECT_EQ(data.ibfs.size(), 6);
+    // data.ibfs[0] is the high-level IBF
+    // data.ibfs[1] is the LOW LEVEL IBF 0;0;0
+    // data.ibfs[2] is the LOW LEVEL IBF 0;0
+    // data.ibfs[3] is the LOW LEVEL IBF 0;1
+    // data.ibfs[4] is the LOW LEVEL IBF 0
+    // data.ibfs[5] is the LOW LEVEL IBF 1
 
-    { // SEQ3 SPECIFIC
-        auto && [hibf_counts, libf_counts] = this->count_kmers(hibf_agent, libf_agent, seq3_specific, config);
+    /* HIGH LEVEL IBF
+     * --------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1, seq2 (merged)
+     * Bin 2: seq2, seq3
+     * Bin 3: split but together: seq1, seq2, seq3
+     * Bin 4: --> belongs to bin 3
+     */
+    EXPECT_EQ(data.ibfs[0].bin_count(), 5u);
+    EXPECT_EQ(data.ibfs[0].bin_size(), 114226);
 
-        size_t expected = std::ranges::distance(seq3_specific | seqan3::views::kmer_hash(seqan3::ungapped{config.k}));
+    /* LOW LEVEL IBF 0;0;0
+     * -------------------
+     * Bin 0: split but together: seq1
+     * Bin 1: --> belongs to bin 0
+     * Bin 2: --> belongs to bin 0
+     * Bin 3: seq2
+     * Bin 4: split but together: seq3
+     * Bin 5: --> belongs to bin 4
+     * Bin 6: split but together: seq2, seq3
+     * Bin 7: --> belongs to bin 6
+     * Bin 8: split but together: seq1, seq2, seq3
+     * Bin 9: --> belongs to bin 8
+     * Bin 10: --> belongs to bin 8
+     * Bin 11: --> belongs to bin 8
+     * Bin 12: --> belongs to bin 8
+     */
+    EXPECT_EQ(data.ibfs[1].bin_count(), 13u);
+    EXPECT_EQ(data.ibfs[1].bin_size(), 92535);
 
-        EXPECT_EQ(hibf_counts[0], 0);
-        EXPECT_EQ(hibf_counts[1], expected);
-        EXPECT_EQ(hibf_counts[2], expected);
-        EXPECT_EQ(hibf_counts[3] + hibf_counts[4] + hibf_counts[5], expected);
-        EXPECT_EQ(hibf_counts[6], expected);
+    /* LOW LEVEL IBF 0;0
+     * -----------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1
+     * Bin 2: seq2
+     * Bin 3: seq3
+     * Bin 4: seq1, seq2
+     */
+    EXPECT_EQ(data.ibfs[2].bin_count(), 5u);
+    EXPECT_EQ(data.ibfs[2].bin_size(), 95321);
 
-        EXPECT_EQ(libf_counts[0], 0);
-        EXPECT_EQ(libf_counts[1], 0);
-        EXPECT_EQ(libf_counts[2] + libf_counts[3] + libf_counts[4], expected);
-        EXPECT_EQ(libf_counts[5], expected);
-    }
+    /* LOW LEVEL IBF 0;1
+     * -----------------
+     * Bin 0: seq1
+     * Bin 1: seq2
+     * Bin 2: seq3
+     */
+    EXPECT_EQ(data.ibfs[3].bin_count(), 3u);
+    EXPECT_EQ(data.ibfs[3].bin_size(), 92734);
+
+    /* LOW LEVEL IBF 0
+     * ---------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1, seq2, seq3 (merged)
+     * Bin 2: seq1, seq2
+     * Bin 3: seq1, seq3
+     * Bin 4: seq2, seq3
+     */
+    EXPECT_EQ(data.ibfs[4].bin_count(), 5u);
+    EXPECT_EQ(data.ibfs[4].bin_size(), 114226);
+
+    /* LOW LEVEL IBF 1
+     * ---------------
+     * Bin 0: seq1
+     * Bin 1: seq2
+     * Bin 2: split but together: seq1, seq2
+     * Bin 3: --> belongs to bin 2
+     */
+    EXPECT_EQ(data.ibfs[5].bin_count(), 4u);
+    EXPECT_EQ(data.ibfs[5].bin_size(), 47561);
+
+    auto unspecific = "ACGATCGACTAGGAGCGATTACGACTGACTACATCTAGCTAGCTAGAGATTCTTCAGAGCTTAGCGATCTCGAGCTATCG"_dna4;
+    auto seq2_specific = "ATATCGATCGAGCGAGGCAGGCAGCGATCGAGCGAGCGCATGCAGCGACTAGCTACGACAGCTACTATCAGCAGCGAGCG"_dna4;
+    auto seq3_specific = "ATCGATCACGATCAGCGAGCGATATCTTATCGTAGGCATCGAGCATCGAGGAGCGATCTATCTATCTATCATCTATCTAT"_dna4;
+
+    // UNSPECIFIC - unspecific region should be found in all bins that include a whole sequence
+    compare_counts(data.ibfs[0], unspecific, {{0}, {1}, {2}, {3,4}}, config);
+    compare_counts(data.ibfs[1], unspecific, {{0, 1, 2}, {3}, {4,5}, {6,7}, {8,9,10,11,12}}, config);
+    compare_counts(data.ibfs[2], unspecific, {{0}, {1}, {2}, {3}, {4}}, config);
+    compare_counts(data.ibfs[3], unspecific, {{0}, {1}, {2}}, config);
+    compare_counts(data.ibfs[4], unspecific, {{0}, {1}, {2}, {3}, {4}}, config);
+    compare_counts(data.ibfs[5], unspecific, {{0}, {1}, {2,3}}, config);
+
+    // SEQ2 SPECIFIC
+    compare_counts(data.ibfs[0], seq2_specific, {{0}, {1}, {2}, {3,4}}, config);
+    compare_counts(data.ibfs[1], seq2_specific, {{3}, {6,7}, {8,9,10,11,12}}, config);
+    compare_counts(data.ibfs[2], seq2_specific, {{0}, {2}, {4}}, config);
+    compare_counts(data.ibfs[3], seq2_specific, {{1}}, config);
+    compare_counts(data.ibfs[4], seq2_specific, {{0}, {1}, {2}, {4}}, config);
+    compare_counts(data.ibfs[5], seq2_specific, {{1}, {2,3}}, config);
+
+    // SEQ3 SPECIFIC
+    compare_counts(data.ibfs[0], seq3_specific, {{0}, {2}, {3,4}}, config);
+    compare_counts(data.ibfs[1], seq3_specific, {{4,5}, {6,7}, {8,9,10,11,12}}, config);
+    compare_counts(data.ibfs[2], seq3_specific, {{0}, {3}}, config);
+    compare_counts(data.ibfs[3], seq3_specific, {{2}}, config);
+    compare_counts(data.ibfs[4], seq3_specific, {{0}, {1}, {3}, {4}}, config);
+    compare_counts(data.ibfs[5], seq3_specific, {}, config);
 }
