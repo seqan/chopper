22728dfd21395b5c42254e9afd18b5a8aced0e1c
smehringer <svenja.mehringer@fu-berlin.de>
2021-02-12 09:35:16 +0100

    wip on search. multi level test passes.

--- test/api/search/chopper_search_test.cpp
+++ test/api/search/chopper_search_test.cpp
@@ -191,177 +191,182 @@ TEST_F(chopper_search_test, first_example)
     EXPECT_EQ(std_cout, expected);
 }
 
-// TEST_F(chopper_search_test, multi_level_example)
-// {
-//     std::string seq1_filename = DATADIR"seq1.fa";
-//     std::string seq2_filename = DATADIR"seq2.fa";
-//     std::string seq3_filename = DATADIR"seq3.fa";
-//     std::string seq12_filename = seq1_filename + ";" + seq2_filename;
-//     std::string seq13_filename = seq1_filename + ";" + seq3_filename;
-//     std::string seq23_filename = seq2_filename + ";" + seq3_filename;
-//     std::string seq123_filename = seq1_filename + ";" + seq2_filename + ";" + seq3_filename;
+TEST_F(chopper_search_test, multi_level_example)
+{
+    std::string seq1_filename = DATADIR"seq1.fa";
+    std::string seq2_filename = DATADIR"seq2.fa";
+    std::string seq3_filename = DATADIR"seq3.fa";
+    std::string seq12_filename = seq1_filename + ";" + seq2_filename;
+    std::string seq13_filename = seq1_filename + ";" + seq3_filename;
+    std::string seq23_filename = seq2_filename + ";" + seq3_filename;
+    std::string seq123_filename = seq1_filename + ";" + seq2_filename + ";" + seq3_filename;
 
-//     seqan3::test::tmp_filename chopper_pack_filename{"small.pack"};
+    seqan3::test::tmp_filename chopper_pack_filename{"small.pack"};
 
-//     // generate data files
-//     {
-//         std::ofstream fout{chopper_pack_filename.get_path()};
-//         fout << "#HIGH_LEVEL_IBF max_bin_id:0\n"
-//              << "#MERGED_BIN_0;0;0 max_bin_id:3\n"
-//              << "#MERGED_BIN_0;0 max_bin_id:4\n"
-//              << "#MERGED_BIN_0;1 max_bin_id:2\n"
-//              << "#MERGED_BIN_0 max_bin_id:0\n"
-//              << "#MERGED_BIN_1 max_bin_id:2\n"
-//              << "#FILES\tBIN_INDICES\tNUMBER_OF_BINS\tEST_MAX_TB_SIZES\n"
-//              << seq1_filename   << "\t0;0;0;0\t1;1;1;3\t1650;350;80;1\n"
-//              << seq2_filename   << "\t0;0;0;3\t1;1;1;1\t1650;350;80;2\n"
-//              << seq3_filename   << "\t0;0;0;4\t1;1;1;2\t1650;350;80;2\n"
-//              << seq23_filename  << "\t0;0;0;6\t1;1;1;2\t1650;350;80;2\n"
-//              << seq123_filename << "\t0;0;0;8\t1;1;1;5\t1650;350;80;2\n"
-//              << seq1_filename   << "\t0;0;1\t1;1;1\t1650;350;40\n"
-//              << seq2_filename   << "\t0;0;2\t1;1;1\t1650;350;50\n"
-//              << seq3_filename   << "\t0;0;3\t1;1;1\t1650;350;80\n"
-//              << seq12_filename  << "\t0;0;4\t1;1;1\t1650;350;100\n"
-//              << seq1_filename   << "\t0;1;0\t1;1;1\t1650;400;6\n"
-//              << seq2_filename   << "\t0;1;1\t1;1;1\t1650;400;7\n"
-//              << seq3_filename   << "\t0;1;2\t1;1;1\t1650;400;7\n"
-//              << seq12_filename  << "\t0;2\t1;1\t1650;200\n"
-//              << seq13_filename  << "\t0;3\t1;1\t1650;300\n"
-//              << seq23_filename  << "\t0;4\t1;1\t1650;400\n"
-//              << seq1_filename   << "\t1;0\t1;1\t2000;31\n"
-//              << seq2_filename   << "\t1;1\t1;1\t2000;32\n"
-//              << seq12_filename  << "\t1;2\t1;2\t2000;32\n"
-//              << seq23_filename  << "\t2\t1\t1200\n"
-//              << seq123_filename << "\t3\t2\t1500\n";
-//     }
+    // generate data files
+    {
+        std::ofstream fout{chopper_pack_filename.get_path()};
+        fout << "#HIGH_LEVEL_IBF max_bin_id:0\n"
+             << "#MERGED_BIN_0;0;0 max_bin_id:3\n"
+             << "#MERGED_BIN_0;0 max_bin_id:4\n"
+             << "#MERGED_BIN_0;1 max_bin_id:2\n"
+             << "#MERGED_BIN_0 max_bin_id:0\n"
+             << "#MERGED_BIN_1 max_bin_id:2\n"
+             << "#FILES\tBIN_INDICES\tNUMBER_OF_BINS\tEST_MAX_TB_SIZES\n"
+             << seq1_filename   << "\t0;0;0;0\t1;1;1;3\t1650;350;80;1\n"
+             << seq2_filename   << "\t0;0;0;3\t1;1;1;1\t1650;350;80;2\n"
+             << seq3_filename   << "\t0;0;0;4\t1;1;1;2\t1650;350;80;2\n"
+             << seq23_filename  << "\t0;0;0;6\t1;1;1;2\t1650;350;80;2\n"
+             << seq123_filename << "\t0;0;0;8\t1;1;1;5\t1650;350;80;2\n"
+             << seq1_filename   << "\t0;0;1\t1;1;1\t1650;350;40\n"
+             << seq2_filename   << "\t0;0;2\t1;1;1\t1650;350;50\n"
+             << seq3_filename   << "\t0;0;3\t1;1;1\t1650;350;80\n"
+             << seq12_filename  << "\t0;0;4\t1;1;1\t1650;350;100\n"
+             << seq1_filename   << "\t0;1;0\t1;1;1\t1650;400;6\n"
+             << seq2_filename   << "\t0;1;1\t1;1;1\t1650;400;7\n"
+             << seq3_filename   << "\t0;1;2\t1;1;1\t1650;400;7\n"
+             << seq12_filename  << "\t0;2\t1;1\t1650;200\n"
+             << seq13_filename  << "\t0;3\t1;1\t1650;300\n"
+             << seq23_filename  << "\t0;4\t1;1\t1650;400\n"
+             << seq1_filename   << "\t1;0\t1;1\t2000;31\n"
+             << seq2_filename   << "\t1;1\t1;1\t2000;32\n"
+             << seq12_filename  << "\t1;2\t1;2\t2000;32\n"
+             << seq23_filename  << "\t2\t1\t1200\n"
+             << seq123_filename << "\t3\t2\t1500\n";
+    }
 
-//     build_config bconfig{};
-//     bconfig.k = 15;
-//     bconfig.chopper_pack_filename = chopper_pack_filename.get_path().string();
+    seqan3::test::tmp_filename build_prefix{"TEST_"};
+    // first execute building to generate the needed input data
+    {
+        const char * argv[] = {"./chopper-build",
+                               "-k", "15",
+                               "-p", chopper_pack_filename.get_path().c_str(),
+                               "-o", build_prefix.get_path().c_str()};
+        int argc = 7;
+        seqan3::argument_parser build_parser{"chopper-build", argc, argv, seqan3::update_notifications::off};
 
-//     build_data bdata{};
+        testing::internal::CaptureStderr();
+        auto parse_res = chopper_build(build_parser);
+        std::string std_cerr = testing::internal::GetCapturedStderr();
+        ASSERT_EQ(parse_res, 0) << std_cerr;
 
-//     create_ibfs_from_chopper_pack(bdata, bconfig);
+        ASSERT_TRUE(std::filesystem::exists(build_prefix.get_path().string() + "HIBF.out"));
+    }
 
-//     // move build data to search data
-//     search_data data{};
-//     for (auto & ibf : bdata.hibf)
-//         data.hibf.push_back(static_cast<seqan3::technical_binning_directory<> &&>(ibf));
+    /* HIGH LEVEL IBF
+     * --------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1, seq2 (merged)
+     * Bin 2: seq2, seq3
+     * Bin 3: split but together: seq1, seq2, seq3
+     * Bin 4: --> belongs to bin 3
+     */
 
-//     data.hibf_bin_levels = std::move(bdata.hibf_bin_levels);
-//     data.user_bins = std::move(bdata.user_bins);
+    /* LOW LEVEL IBF 0;0;0
+     * -------------------
+     * Bin 0: split but together: seq1
+     * Bin 1: --> belongs to bin 0
+     * Bin 2: --> belongs to bin 0
+     * Bin 3: seq2
+     * Bin 4: split but together: seq3
+     * Bin 5: --> belongs to bin 4
+     * Bin 6: split but together: seq2, seq3
+     * Bin 7: --> belongs to bin 6
+     * Bin 8: split but together: seq1, seq2, seq3
+     * Bin 9: --> belongs to bin 8
+     * Bin 10: --> belongs to bin 8
+     * Bin 11: --> belongs to bin 8
+     * Bin 12: --> belongs to bin 8
+     */
 
-//     search_config config{};
-//     config.k = bconfig.k;
+    /* LOW LEVEL IBF 0;0
+     * -----------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1
+     * Bin 2: seq2
+     * Bin 3: seq3
+     * Bin 4: seq1, seq2
+     */
 
-//     /* HIGH LEVEL IBF
-//      * --------------
-//      * Bin 0: seq1, seq2, seq3 (merged)
-//      * Bin 1: seq1, seq2 (merged)
-//      * Bin 2: seq2, seq3
-//      * Bin 3: split but together: seq1, seq2, seq3
-//      * Bin 4: --> belongs to bin 3
-//      */
+    /* LOW LEVEL IBF 0;1
+     * -----------------
+     * Bin 0: seq1
+     * Bin 1: seq2
+     * Bin 2: seq3
+     */
 
-//     /* LOW LEVEL IBF 0;0;0
-//      * -------------------
-//      * Bin 0: split but together: seq1
-//      * Bin 1: --> belongs to bin 0
-//      * Bin 2: --> belongs to bin 0
-//      * Bin 3: seq2
-//      * Bin 4: split but together: seq3
-//      * Bin 5: --> belongs to bin 4
-//      * Bin 6: split but together: seq2, seq3
-//      * Bin 7: --> belongs to bin 6
-//      * Bin 8: split but together: seq1, seq2, seq3
-//      * Bin 9: --> belongs to bin 8
-//      * Bin 10: --> belongs to bin 8
-//      * Bin 11: --> belongs to bin 8
-//      * Bin 12: --> belongs to bin 8
-//      */
+    /* LOW LEVEL IBF 0
+     * ---------------
+     * Bin 0: seq1, seq2, seq3 (merged)
+     * Bin 1: seq1, seq2, seq3 (merged)
+     * Bin 2: seq1, seq2
+     * Bin 3: seq1, seq3
+     * Bin 4: seq2, seq3
+     */
 
-//     /* LOW LEVEL IBF 0;0
-//      * -----------------
-//      * Bin 0: seq1, seq2, seq3 (merged)
-//      * Bin 1: seq1
-//      * Bin 2: seq2
-//      * Bin 3: seq3
-//      * Bin 4: seq1, seq2
-//      */
+    /* LOW LEVEL IBF 1
+     * ---------------
+     * Bin 0: seq1
+     * Bin 1: seq2
+     * Bin 2: split but together: seq1, seq2
+     * Bin 3: --> belongs to bin 2
+     */
 
-//     /* LOW LEVEL IBF 0;1
-//      * -----------------
-//      * Bin 0: seq1
-//      * Bin 1: seq2
-//      * Bin 2: seq3
-//      */
+    // generate query file
+    seqan3::test::tmp_filename query_filename{"queries.fa"};
+    {
+        std::ofstream fout{query_filename.get_path()};
+        fout << ">unspecific\n"
+             << "ACGATCGACTAGGAGCGATTACGACTGACTACATCTAGCTAGCTAGAGATTCTTCAGAGCTTAGCGATCTCGAGCTATCG\n"
+             << ">seq2_specific\n"
+             << "ATATCGATCGAGCGAGGCAGGCAGCGATCGAGCGAGCGCATGCAGCGACTAGCTACGACAGCTACTATCAGCAGCGAGCG\n"
+             << ">seq3_specific\n"
+             << "ATCGATCACGATCAGCGAGCGATATCTTATCGTAGGCATCGAGCATCGAGGAGCGATCTATCTATCTATCATCTATCTAT\n";
+    }
 
-//     /* LOW LEVEL IBF 0
-//      * ---------------
-//      * Bin 0: seq1, seq2, seq3 (merged)
-//      * Bin 1: seq1, seq2, seq3 (merged)
-//      * Bin 2: seq1, seq2
-//      * Bin 3: seq1, seq3
-//      * Bin 4: seq2, seq3
-//      */
+    std::string const index_filename{build_prefix.get_path().string() + "HIBF.out"};
+    ASSERT_TRUE(std::filesystem::exists(index_filename));
+    const char * argv[] = {"./chopper-search",
+                           "-k", "15",
+                           "-i", index_filename.c_str(),
+                           "-q", query_filename.get_path().c_str()};
+    int argc = 7;
+    seqan3::argument_parser search_parser{"chopper-search", argc, argv, seqan3::update_notifications::off};
 
-//     /* LOW LEVEL IBF 1
-//      * ---------------
-//      * Bin 0: seq1
-//      * Bin 1: seq2
-//      * Bin 2: split but together: seq1, seq2
-//      * Bin 3: --> belongs to bin 2
-//      */
+    testing::internal::CaptureStderr();
+    testing::internal::CaptureStdout();
+    auto parse_res = chopper_search(search_parser);
+    std::string std_cerr = testing::internal::GetCapturedStderr();
+    std::string std_cout = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(parse_res, 0) << std_cerr;
 
-//     auto unspecific = "ACGATCGACTAGGAGCGATTACGACTGACTACATCTAGCTAGCTAGAGATTCTTCAGAGCTTAGCGATCTCGAGCTATCG"_dna4;
-//     auto seq2_specific = "ATATCGATCGAGCGAGGCAGGCAGCGATCGAGCGAGCGCATGCAGCGACTAGCTACGACAGCTACTATCAGCAGCGAGCG"_dna4;
-//     auto seq3_specific = "ATCGATCACGATCAGCGAGCGATATCTTATCGTAGGCATCGAGCATCGAGGAGCGATCTATCTATCTATCATCTATCTAT"_dna4;
+    std::string expected
+    {
+        "#0\t" + seq12_filename + "\n"
+        "#1\t" + seq2_filename + "\n"
+        "#2\t" + seq1_filename + "\n"
+        "#3\t" + seq3_filename + "\n"
+        "#4\t" + seq23_filename + "\n"
+        "#5\t" + seq123_filename + "\n"
+        "#6\t" + seq1_filename + "\n"
+        "#7\t" + seq2_filename + "\n"
+        "#8\t" + seq3_filename + "\n"
+        "#9\t" + seq3_filename + "\n"
+        "#10\t" + seq1_filename + "\n"
+        "#11\t" + seq2_filename + "\n"
+        "#12\t" + seq12_filename + "\n"
+        "#13\t" + seq13_filename + "\n"
+        "#14\t" + seq23_filename + "\n"
+        "#15\t" + seq12_filename + "\n"
+        "#16\t" + seq1_filename + "\n"
+        "#17\t" + seq2_filename + "\n"
+        "#18\t" + seq23_filename + "\n"
+        "#19\t" + seq123_filename + "\n"
+        "#QUERY_NAME\tUSER_BINS\n"
+        "unspecific\t0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19\n"
+        "seq2_specific\t0,1,4,5,7,11,12,14,15,17,18,19\n"
+        "seq3_specific\t3,4,5,8,9,13,14,18,19\n"
+    };
 
-//     { // unspecific
-//         std::vector<size_t> kmers = compute_kmers(unspecific, config);
-//         std::unordered_set<std::pair<int32_t, uint32_t>, pair_hash> result{};
-
-//         search(result, kmers, data, config, 0); // start at top level ibf
-
-//         this->compare_result(result, {
-//             /*high-level IBF      */ {0,2},{0,4},
-//             /*LOW LEVEL IBF 0;0;0 */ {1,2},{1,3},{1,5},{1,7},{1,12},
-//             /*LOW LEVEL IBF 0;0   */ {2,1},{2,2},{2,3},{2,4},
-//             /*LOW LEVEL IBF 0;1   */ {3,0},{3,1},{3,2},
-//             /*LOW LEVEL IBF 0     */ {4,2},{4,3},{4,4},
-//             /*LOW LEVEL IBF 1     */ {5,0},{5,1},{5,3}
-//         });
-//     }
-
-//     { // seq2_specific
-//         std::vector<size_t> kmers = compute_kmers(seq2_specific, config);
-//         std::unordered_set<std::pair<int32_t, uint32_t>, pair_hash> result{};
-
-//         search(result, kmers, data, config, 0); // start at top level ibf
-
-//         this->compare_result(result, {
-//             /*high-level IBF      */ {0,2},{0,4},
-//             /*LOW LEVEL IBF 0;0;0 */ {1,3},{1,7},{1,12},
-//             /*LOW LEVEL IBF 0;0   */ {2,2},{2,4},
-//             /*LOW LEVEL IBF 0;1   */ {3,1},
-//             /*LOW LEVEL IBF 0     */ {4,2},{4,4},
-//             /*LOW LEVEL IBF 1     */ {5,1},{5,3}
-//         });
-//     }
-
-//     { // seq3_specific
-//         std::vector<size_t> kmers = compute_kmers(seq3_specific, config);
-//         std::unordered_set<std::pair<int32_t, uint32_t>, pair_hash> result{};
-
-//         search(result, kmers, data, config, 0); // start at top level ibf
-
-//         this->compare_result(result, {
-//             /*high-level IBF      */ {0,2},{0,4},
-//             /*LOW LEVEL IBF 0;0;0 */ {1,5},{1,7},{1,12},
-//             /*LOW LEVEL IBF 0;0   */ {2,3},
-//             /*LOW LEVEL IBF 0;1   */ {3,2},
-//             /*LOW LEVEL IBF 0     */ {4,3},{4,4}
-//             /*LOW LEVEL IBF 1     */
-//         });
-//     }
-// }
+    EXPECT_EQ(std_cout, expected);
+}
