7f853348b9b734aa91dcc98b1789f9390edb0fc3
Enrico Seiler <enrico.seiler@hotmail.de>
2021-02-23 10:59:47 +0100

    [MISC] Rewrite and comment bin size estimation

--- include/chopper/build/compute_bin_size.hpp
+++ include/chopper/build/compute_bin_size.hpp
@@ -4,6 +4,15 @@
 
 size_t compute_bin_size(build_config const & config, size_t const number_of_kmers_to_be_stored)
 {
-    return (size_t)std::ceil((-(double)(config.hash_funs * number_of_kmers_to_be_stored)) /
-                   (std::log(1 - std::pow(10.0, std::log10(config.FPR) / config.hash_funs))));
+    return std::ceil( - static_cast<double>(number_of_kmers_to_be_stored * config.hash_funs) /
+                     std::log(1 - std::exp(std::log(config.FPR) / config.hash_funs)));
 }
+
+
+// -NUM_ELEM*HASHES
+// ----------------------  = SIZE
+// LN(1-FPR^(1/HASHES))
+
+// -NUM_ELEMS*HASHES
+// -----------------------
+// LN(1 - e^(LN(FPR) / HASHES) )
