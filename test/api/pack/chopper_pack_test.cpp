0cc28f36d8577897121c08a7da910a339e25b8e9
Enrico Seiler <enrico.seiler@hotmail.de>
2021-07-16 11:45:41 +0200

    [TEST] add pack test with HLLs

--- test/api/pack/chopper_pack_test.cpp
+++ test/api/pack/chopper_pack_test.cpp
@@ -7,6 +7,7 @@
 #include <chopper/pack/chopper_pack.hpp>
 
 #include "../api_test.hpp"
+#include "print_debug_file.hpp"
 
 TEST(chopper_pack_test, small_example)
 {
@@ -52,42 +53,6 @@ TEST(chopper_pack_test, small_example)
     std::ifstream output_file{output_filename.get_path()};
     std::string const output_file_str((std::istreambuf_iterator<char>(output_file)), std::istreambuf_iterator<char>());
     EXPECT_EQ(output_file_str, expected_file);
-}
-
-void print_debug_file(std::filesystem::path const & path, size_t const column_width = 18u)
-{
-    std::ios::fmtflags const original_flags = std::cout.setf(std::ios::left, std::ios::adjustfield);
-
-    std::ifstream packing_file{path};
-
-    std::string buffer{};
-    buffer.reserve(column_width);
-    auto flush_buffer = [&buffer, &column_width] ()
-    {
-        std::cout.width(column_width);
-        std::cout << buffer;
-        buffer.clear();
-    };
-
-    for (auto it = std::istreambuf_iterator<char>(packing_file); it != std::istreambuf_iterator<char>(); ++it)
-    {
-        char const letter{*it};
-        switch (letter)
-        {
-            case '\n':
-                flush_buffer();
-                std::cout << '\n';
-                break;
-            case '\t':
-                flush_buffer();
-                break;
-            default:
-                buffer += letter;
-                break;
-        }
-    }
-
-    std::cout.setf(original_flags);
 }
 
 TEST(chopper_pack_test, small_example_debug)
