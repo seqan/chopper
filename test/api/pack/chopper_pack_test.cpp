b867835dc79f0ba99218ae19885c595a2801d770
Enrico Seiler <enrico.seiler@hotmail.de>
2021-07-16 09:53:11 +0200

    [MISC,TEST] Add function for pretty-printing packing file

--- test/api/pack/chopper_pack_test.cpp
+++ test/api/pack/chopper_pack_test.cpp
@@ -54,6 +54,42 @@ TEST(chopper_pack_test, small_example)
     EXPECT_EQ(output_file_str, expected_file);
 }
 
+void print_debug_file(std::filesystem::path const & path, size_t const column_width = 18u)
+{
+    std::ios::fmtflags const original_flags = std::cout.setf(std::ios::left, std::ios::adjustfield);
+
+    std::ifstream packing_file{path};
+
+    std::string buffer{};
+    buffer.reserve(column_width);
+    auto flush_buffer = [&buffer, &column_width] ()
+    {
+        std::cout.width(column_width);
+        std::cout << buffer;
+        buffer.clear();
+    };
+
+    for (auto it = std::istreambuf_iterator<char>(packing_file); it != std::istreambuf_iterator<char>(); ++it)
+    {
+        char const letter{*it};
+        switch (letter)
+        {
+            case '\n':
+                flush_buffer();
+                std::cout << '\n';
+                break;
+            case '\t':
+                flush_buffer();
+                break;
+            default:
+                buffer += letter;
+                break;
+        }
+    }
+
+    std::cout.setf(original_flags);
+}
+
 TEST(chopper_pack_test, small_example_debug)
 {
     seqan3::test::tmp_filename data_filename{"data.tsv"};
@@ -98,4 +134,5 @@ TEST(chopper_pack_test, small_example_debug)
     std::ifstream output_file{output_filename.get_path()};
     std::string const output_file_str((std::istreambuf_iterator<char>(output_file)), std::istreambuf_iterator<char>());
     EXPECT_EQ(output_file_str, expected_file);
+    // print_debug_file(output_filename.get_path());
 }
