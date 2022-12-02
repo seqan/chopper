c200bec6f69006cca21a1cc9e7512cf0b90b0cd5
Enrico Seiler <enrico.seiler@hotmail.de>
2022-12-02 13:53:13 +0100

    [TEST] Enable warnings

--- test/cmake/chopper_require_test.cmake
+++ test/cmake/chopper_require_test.cmake
@@ -8,26 +8,32 @@
 cmake_minimum_required (VERSION 3.15)
 
 # Exposes the google-test targets `gtest` and `gtest_main`.
+# CMake 3.24: https://cmake.org/cmake/help/latest/module/FetchContent.html#variable:FETCHCONTENT_TRY_FIND_PACKAGE_MODE
 macro (chopper_require_test)
     enable_testing ()
 
-    set (CHOPPER_GTEST_GIT_TAG
-         "8d51dc50eb7e7698427fed81b85edad0e032112e"
-         CACHE STRING "googletest commit to use")
+    set (gtest_version "1.12.1")
+    set (gtest_git_tag "release-${gtest_version}")
 
-    message (STATUS "Fetch Google Test:")
+    find_package (GTest ${gtest_version} EXACT QUIET)
 
-    include (FetchContent)
-    FetchContent_Declare (
-        gtest_fetch_content
-        GIT_REPOSITORY "https://github.com/google/googletest.git"
-        GIT_TAG "${CHOPPER_GTEST_GIT_TAG}")
-    option (BUILD_GMOCK "" OFF)
-    FetchContent_MakeAvailable (gtest_fetch_content)
+    if (NOT GTest_FOUND)
+        message (STATUS "Fetching Google Test ${gtest_version}")
+
+        include (FetchContent)
+        FetchContent_Declare (gtest_fetch_content
+                              GIT_REPOSITORY "https://github.com/google/googletest.git"
+                              GIT_TAG "${gtest_git_tag}"
+        )
+        option (BUILD_GMOCK "" OFF)
+        option (INSTALL_GTEST "" OFF)
+        FetchContent_MakeAvailable (gtest_fetch_content)
+    else ()
+        message (STATUS "Found Google Test ${gtest_version}")
+    endif ()
 
     if (NOT TARGET gtest_build)
         add_custom_target (gtest_build DEPENDS gtest_main gtest)
-        target_compile_options ("gtest_main" PUBLIC "-w")
-        target_compile_options ("gtest" PUBLIC "-w")
     endif ()
+
 endmacro ()
