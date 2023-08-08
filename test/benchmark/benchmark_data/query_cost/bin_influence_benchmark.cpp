4f21449dc77aea30a05b6f86c52d6451b8615a1e
Enrico Seiler <enrico.seiler@hotmail.de>
2023-08-08 15:13:58 +0200

    [MISC] Add copyright header

--- test/benchmark/benchmark_data/query_cost/bin_influence_benchmark.cpp
+++ test/benchmark/benchmark_data/query_cost/bin_influence_benchmark.cpp
@@ -1,9 +1,9 @@
-// --------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------
 // Copyright (c) 2006-2023, Knut Reinert & Freie Universität Berlin
 // Copyright (c) 2016-2023, Knut Reinert & MPI für molekulare Genetik
 // This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
-// shipped with this file and also available at: https://github.com/seqan/raptor/blob/main/LICENSE.md
-// --------------------------------------------------------------------------------------------------
+// shipped with this file and also available at: https://github.com/seqan/chopper/blob/main/LICENSE.md
+// ---------------------------------------------------------------------------------------------------
 
 // clang-format off
 // https://github.com/seqan/raptor/blob/main/test/performance/bin_influence_benchmark.cpp
